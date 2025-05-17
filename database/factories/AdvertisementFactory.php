65ab8582fea853fea1094b2fb2219b13656e7ea0
Hiran Nuwanpriya <hirannuwanpriya@gmail.com>
2025-05-17 19:55:37 +0530

    add db seeder to create sample advertisements

---
+++ database/factories/AdvertisementFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Advertisement;
+use App\Models\AdvertisementCategory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Advertisement>
+ */
+class AdvertisementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+            'category_id' => AdvertisementCategory::factory(),
+            'content' => $this->faker->paragraph,
+            'opening_hours' => $this->faker->time('H:i') . ' - ' . $this->faker->time('H:i'),
+            'location' => $this->faker->address,
+            'website_url' => $this->faker->url,
+            'status' => $this->faker->numberBetween(0, 1),
+            'published_at' => $this->faker->dateTime,
+            'modified_at' => $this->faker->dateTime,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Advertisement;
+use App\Models\AdvertisementCategory;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +21,33 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $categories = [
+            'Electronics',
+            'Furniture',
+            'Clothing',
+            'Books',
+            'Toys',
+            'Sports Equipment',
+            'Automotive',
+            'Real Estate',
+            'Health & Beauty',
+            'Miscellaneous'
+        ];
+
+        // Create categories
+        foreach ($categories as $category) {
+            AdvertisementCategory::create([
+                'name' => $category,
+                'slug' => strtolower(str_replace(' ', '-', $category)),
+                'description' => "Description for $category",
+                'status' => 1,
+            ]);
+
+            // Create advertisements for each category
+            Advertisement::factory(5)->create([
+                'category_id' => AdvertisementCategory::where('name', $category)->first()->id,
+            ]);
+        }
     }
 }
